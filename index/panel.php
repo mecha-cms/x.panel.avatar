7a4a9b3756190f026b38543e61660f01de43c9d5
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-02-06 11:28:14 +0700

    Update

--- index/panel.php
+++ index/panel.php
@@ -1,14 +1,15 @@
 <?php
 
+$file = $_['file'];
 $is_page = 0 === strpos($_['type'] . '/', 'page/');
-if (!$_['type'] && ($file = $_['file'])) {
+if ($file && !isset($_['type'])) {
     if (false !== strpos(',archive,draft,page,', ',' . pathinfo($file, PATHINFO_EXTENSION) . ',')) {
         $is_page = true;
     }
 }
 
-if ((0 === strpos($_['path'] . '/', 'comment/') || 0 === strpos($_['path'] . '/', 'user/')) && $is_page) {
-    State::set("x.panel\\.image.folder", LOT . D . 'asset' . D . 'user' . D . $user->name . D . 'avatar');
+if ($is_page && (0 === strpos($_['path'] . '/', 'comment/') || 0 === strpos($_['path'] . '/', 'user/'))) {
+    State::set("x.panel\\.image.folder", LOT . D . 'asset' . D . 'user' . D . ($file ? pathinfo($file, PATHINFO_FILENAME) : $user->name) . D . 'avatar');
     State::set("x.panel\\.image.key", 'avatar');
     State::set("x.panel\\.image.name", date('Y-m-d'));
     State::set("x.panel\\.image.title", 'Avatar');
