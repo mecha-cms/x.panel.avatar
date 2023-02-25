48da4f7edce1f498871829fce212c68d7a20299e
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-02-25 12:04:11 +0700

    Resize and Crop if Feature Available

--- index/panel.php
+++ index/panel.php
@@ -10,7 +10,9 @@ if ($file && !isset($_['type'])) {
 
 if ($is_page && (0 === strpos($_['path'] . '/', 'comment/') || 0 === strpos($_['path'] . '/', 'user/'))) {
     State::set("x.panel\\.image.folder", LOT . D . 'asset' . D . 'user' . D . ($file ? pathinfo($file, PATHINFO_FILENAME) : $user->name) . D . 'avatar');
+    State::set("x.panel\\.image.height", 400);
     State::set("x.panel\\.image.key", 'avatar');
     State::set("x.panel\\.image.name", date('Y-m-d'));
     State::set("x.panel\\.image.title", 'Avatar');
+    State::set("x.panel\\.image.width", 400);
 }
